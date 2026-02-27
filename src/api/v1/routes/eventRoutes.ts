8df2bc454c723db505606dc7de161ffe7858b2ad
Keith <krobles3@rrc.ca>
2026-02-27 00:08:11 -0600

    Changed routes

---
+++ src/api/v1/routes/eventRoutes.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { getAllEventsService, getEventByIdService, createEventService, updateEventService, deleteEventService } from "../services/eventService"
+import { validateRequest } from "../middleware/validate";
+import * as postController from "../controllers/postController";
+import { postSchemas } from "../validation/postSchemas";
+
+const router = express.Router();
+
+router.get('/events', getAllEvents);
+router.get('/events/:id', getEventById);
+router.post('/events', createEvent);
+router.put('/events/:id', updateEvent);
+router.delete('/events/:id', deleteEvent);
+
+export default router;
