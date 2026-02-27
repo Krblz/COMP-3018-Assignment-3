b3bc66c136919eb03c56448ca7cd564a490204e8
Keith <krobles3@rrc.ca>
2026-02-27 00:13:36 -0600

    Added Event Controller

---
+++ src/api/v1/controllers/eventController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { getAllEventsService, getEventByIdService, createEventService, updateEventService, deleteEventService } from "../services/eventService"
+import {HTTP_STATUS} from "../../../constants/httpConstants"
+
+export const getAllEvents = (req: Request, res: Response) => {
+    // Logic to get all items
+
+    let result = getAllEventsService()
+    res.status(HTTP_STATUS.OK).json(result);
+};
+
+export const getEventById = (req: Request, res: Response) => {
+    let id = Number(req.params.id)
+
+    if (Number.isNaN(id)) {
+        res.status(HTTP_STATUS.BAD_REQUEST).json({ error: "ID must be a Number" });
+        return;
+    }
+
+    let result = getEventByIdService(id)
+
+    if (result === undefined) {
+        res.status(HTTP_STATUS.NOT_FOUND).json({ error: "Event not found" });
+    }
+
+    res.status(HTTP_STATUS.OK).json(result);
+};
+
+export const createEvent = (req: Request, res: Response) => {
+    let newEvent = req.body
+
+    let result = createEventService(newEvent)
+    res.status(200).json(result);
+};
+
+export const updateEvent = (req: Request, res: Response) => {
+    let newEvent = req.body
+    let id = Number(req.params.id)
+
+    let result = updateEventService(id, newEvent)
+    res.status(200).json(result);
+};
+
+export const deleteEvent = (req: Request, res: Response) => {
+    let id = Number(req.params.id)
+    let result = deleteEventService(id)
+    res.status(200).json(result);
+};
