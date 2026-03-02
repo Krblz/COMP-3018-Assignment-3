da5305f482476b0b4c22a0ea8e1674b08f4ebca9
Keith <krobles3@rrc.ca>
2026-03-01 19:14:23 -0600

    Updated eventRoutes so that it validates through middleware

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import { getAllEvents, getEventById, createEvent, updateEvent, deleteEvent } from "../controllers/eventController"
-// import { validateRequest } from "../middleware/validate";
-// import * as postController from "../controllers/postController";
-// import { postSchemas } from "../validation/postSchemas";
+import { validateRequest } from "../middleware/validate";
+import { eventSchemas } from "../validation/eventSchemas";
 
 const router: Router = Router();
 
-router.get('/events', getAllEvents);
-router.get('/events/:id', getEventById);
-router.post('/events', createEvent);
-router.put('/events/:id', updateEvent);
-router.delete('/events/:id', deleteEvent);
+router.get('/events', validateRequest(eventSchemas.list), getAllEvents);
+router.get('/events/:id', validateRequest(eventSchemas.getById), getEventById);
+router.post('/events', validateRequest(eventSchemas.create), createEvent);
+router.put('/events/:id', validateRequest(eventSchemas.update), updateEvent);
+router.delete('/events/:id', validateRequest(eventSchemas.delete), deleteEvent);
 
 export default router;
